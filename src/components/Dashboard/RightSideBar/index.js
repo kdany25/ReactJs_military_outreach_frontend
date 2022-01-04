ba6f04902d408c6cd9c9d703d18a8f4eb1012c1e
kdany25 <kabadany25@gmail.com>
2022-01-04 18:39:36 +0200

    adding side lines

--- src/components/Dashboard/RightSideBar/index.js
+++ src/components/Dashboard/RightSideBar/index.js
@@ -1,12 +1,70 @@
 import React from "react";
-import './Style/rightbar.css'
+import "./Style/rightbar.css";
 
-const RightBar = ()=>{
-    return(
-        <div className="right">
-            dsbfdbskfbjksdfjsdf
+const RightBar = () => {
+  return (
+    <div className="right">
+      <div className="top">
+        <button id="menu-btn">
+          <span class="material-icons-sharp">linear_scale</span>
+        </button>
+        <div className="theme-toggler">
+          <span class="material-icons-sharp active">light_mode</span>
+          <span class="material-icons-sharp">dark_mode</span>
         </div>
-    )
-}
+        <div className="profile">
+          <div className="info">
+            <p>
+              <b>k.dany</b>
+            </p>
+            <small className="text-muted">Admin</small>
+          </div>
+          <div className="profile-photo">
+            <img src="profile.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="recent-updated">
+        <h2>Recent Updates</h2>
+        <div className="updates">
+          <div className="update">
+            <div className="profile-photo">
+              <img src="recent1.PNG" />
+            </div>
+            <div className="message">
+              <p>
+                <b>RDF </b>Lorem Ipsum es simplemente el texto de relleno de las
+              </p>
+              <small className="texted-muted">5 hours ago</small>
+            </div>
+          </div>
+          <div className="update">
+            <div className="profile-photo">
+              <img src="recent2.PNG" />
+            </div>
+            <div className="message">
+              <p>
+                <b>RDF </b>Lorem Ipsum es simplemente el texto de relleno de las
+              </p>
+              <small className="texted-muted">2 day ago</small>
+            </div>
+          </div>
+          <div className="update">
+            <div className="profile-photo">
+              <img src="recent3.PNG" />
+            </div>
+            <div className="message">
+              <p>
+                <b>RDF </b>Lorem Ipsum es simplemente el texto de relleno de las
+              </p>
+              <small className="texted-muted">10 hours ago</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
 
 export default RightBar;
